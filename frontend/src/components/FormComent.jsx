9ffac19af42d83fe431af072956833f3ee3a72c5
jeki18ros-gif <jeki18ros@gmail.com>
2025-11-29 11:00:45 -0500

    feat: :bug: importacion lista

--- frontend/src/components/FormComent.jsx
+++ frontend/src/components/FormComent.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useMemo, useEffect, useCallback } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
-import { supabase } from '../supabase/supabase';
+import { supabase } from '../supabase/supabaseClient';
 
 import {
   XMarkIcon,
